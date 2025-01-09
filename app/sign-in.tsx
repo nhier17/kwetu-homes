3ede19bdb774a36a8c37d9b91dfe17fdce9046c7
Abraham <abrahamnhier97@gmail.com>
2025-01-09 09:44:51 +0300

    routes setup

---
+++ app/sign-in.tsx
@@ -0,0 +1,13 @@
+import { Text, View } from 'react-native'
+import React from 'react'
+
+const Auth = () => {
+  return (
+    <View>
+      <Text>sign-in</Text>
+    </View>
+  )
+}
+
+export default Auth
+
