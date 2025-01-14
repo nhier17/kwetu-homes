b8e7057b5d08aea36b777a5d62ad6e4b7e2d58ed
Abraham <abrahamnhier97@gmail.com>
2025-01-14 17:19:32 +0300

    property data config

--- lib/appwrite.ts
+++ lib/appwrite.ts
@@ -102,3 +102,73 @@ export async function getCurrentUser() {
         return null;
     }
 }
+
+//get latest properties
+export async function getLatestProperties() {
+    try {
+        const result = await databases.listDocuments(
+            config.databaseId!,
+            config.propertiesCollectionId!,
+            [Query.orderAsc("$createdAt"), Query.limit(5)]
+        );
+        return result.documents;
+    } catch (error) {
+        console.error(error);
+        return [];
+    }
+}
+
+//get properties
+export async function getProperties({
+  filter,
+  query,
+  limit,
+}: {
+  filter: string;
+  query: string;
+  limit?: number;
+}) {
+  try {
+    const buildQuery = [Query.orderDesc("$createdAt")];
+
+    if(filter && filter !== "All") 
+      buildQuery.push(Query.equal("type", filter));
+
+    if (query)
+      buildQuery.push(
+        Query.or([
+          Query.search("name", query),
+          Query.search("address", query),
+          Query.search("type", query),
+        ])
+      );
+
+      if(limit) buildQuery.push(Query.limit(limit));
+
+      const result = await databases.listDocuments(
+        config.databaseId!,
+        config.propertiesCollectionId!,
+        buildQuery
+      );
+
+      return result.documents;
+  } catch (error) {
+    console.error(error);
+    return []
+  }
+}
+
+// get property by id
+export async function getPropertyById({ id }: { id: string }) {
+ try {
+    const result = await databases.getDocument(
+      config.databaseId!,
+      config.propertiesCollectionId!,
+      id
+    );
+    return result
+ } catch (error) {
+  console.error(error);
+  return null;
+ }
+}
--- lib/global-provider.tsx
+++ lib/global-provider.tsx
@@ -2,13 +2,13 @@ import React, { createContext, useContext, ReactNode } from "react";
 
 import { getCurrentUser } from "./appwrite";
 import { useAppwrite } from "./useAppwrite";
-import { Redirect } from "expo-router";
+
 
 interface GlobalContextType {
   isLogged: boolean;
   user: User | null;
   loading: boolean;
-  refetch: () => void;
+  refetch: (newParams?: Record<string, string | number>) => Promise<void>;
 }
 
 interface User {
